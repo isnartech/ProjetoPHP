8b7507ad7fc3373924e15f944e7a2ce872ea3b11
Isnar Santana <48757992+isnartech@users.noreply.github.com>
2024-05-13 12:49:13 -0300

    subindoarquivos
    teste de site beta

---
+++ cliente2/config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'u660601093_raicesta';
+    $dbPassword = '@Fatalbash926';
+    $dbName = 'u660601093_cestabasica';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+    // {
+    //     echo "Conexão efetuada com sucesso";
+    // }
+
+?>
